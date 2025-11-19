cc3fdb3767423f0025ece87fba7abe8d20b69323
Mina R <minarumang@icloud.com>
2025-11-19 21:00:08 +0100

    Add pages and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,19 @@
-import ProjectInfo from "./components/ProjectInfo"
-import FormSS from "./assets/FormSS.png"
-import ResponsiveNavigationSS from "./assets/responsive-navigation-SS.png"
-import bootstrapSS from "./assets/bootstrap-SS.png"
-import "./normalize.css"
-
+import "./normalize.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Portfolio from "./components/Portfolio.jsx";
+import Navbar from "./Navbar.jsx";
 function App() {
     return (
         <>
-            <ProjectInfo 
-                name="Form" 
-                img={FormSS}
-                link="https://github.com/MiniMinaa/Form"
-                list=<> 
-                    <li>HTML</li> 
-                    <li>CSS</li>
-                </> 
-                summary="I built a clean, responsive web form using HTML and CSS. I designed the layout, styling, and user experience, focusing on accessibility and consistency. Throughout the project, I improved form structure, solved alignment issues, and ensured the design works well across all devices."
-                ></ProjectInfo>
-            <ProjectInfo 
-                name="Responsive Navigation" 
-                img={ResponsiveNavigationSS}
-                link="https://github.com/MiniMinaa/Form"
-                list=<> 
-                    <li>HTML</li> 
-                    <li>CSS</li>
-                </> 
-                summary="I built a clean, responsive nav menu using HTML and CSS. I designed the layout, styling, and user experience, focusing on clarity and accessibility. Throughout the project, I refined menu structure, solved alignment issues, and ensured the navigation works smoothly across all devices."
-            ></ProjectInfo>
-            <ProjectInfo
-                name="Bootstrap" 
-                img={bootstrapSS}
-                link="https://github.com/MiniMinaa/Form"
-                list=<> 
-                    <li>HTML</li> 
-                    <li>CSS</li>
-                </> 
-                summary="I built a responsive interface using Bootstrap, designing the layout, components, and styling for a consistent user experience. Throughout the project, I explored Bootstrap’s grid and utilities, solved layout issues, and ensured the design works well across all devices."
-            ></ProjectInfo>
+            <BrowserRouter>
+            <Navbar></Navbar>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} /> 
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,37 @@
+function Portfolio() {
+    return (
+        <>
+            <ProjectInfo 
+                name="Form" 
+                img={FormSS}
+                link="https://github.com/MiniMinaa/Form"
+                list=<> 
+                    <li>HTML</li> 
+                    <li>CSS</li>
+                </> 
+                summary="I built a clean, responsive web form using HTML and CSS. I designed the layout, styling, and user experience, focusing on accessibility and consistency. Throughout the project, I improved form structure, solved alignment issues, and ensured the design works well across all devices."
+                ></ProjectInfo>
+            <ProjectInfo 
+                name="Responsive Navigation" 
+                img={ResponsiveNavigationSS}
+                link="https://github.com/MiniMinaa/Form"
+                list=<> 
+                    <li>HTML</li> 
+                    <li>CSS</li>
+                </> 
+                summary="I built a clean, responsive nav menu using HTML and CSS. I designed the layout, styling, and user experience, focusing on clarity and accessibility. Throughout the project, I refined menu structure, solved alignment issues, and ensured the navigation works smoothly across all devices."
+            ></ProjectInfo>
+            <ProjectInfo
+                name="Bootstrap" 
+                img={bootstrapSS}
+                link="https://github.com/MiniMinaa/Form"
+                list=<> 
+                    <li>HTML</li> 
+                    <li>CSS</li>
+                </> 
+                summary="I built a responsive interface using Bootstrap, designing the layout, components, and styling for a consistent user experience. Throughout the project, I explored Bootstrap’s grid and utilities, solved layout issues, and ensured the design works well across all devices."
+            ></ProjectInfo>
+        </>
+    )
+}
+export default Portfolio
