9c9f889b3c281c3fa115275e7aa2adc9fb431206
Mina R <minarumang@icloud.com>
2025-11-27 08:43:06 +0100

    add searchbar

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,15 +1,18 @@
 import "./pages.css"
 
 function Popup(props) {
-    const { name, img, summary, tech, link } = props;
+    const { name, img, summary, tech, link, onClose } = props;
 
     return (
         <div className="popup-container">
-            <h1 className="pheader">{name}</h1>
+            <div className="closebutton">
+                <h1 className="pheader">{name}</h1>
+                <button className="closepopup" onClick={onClose} >X</button>
+            </div>
             <img className="pimg" src={img} alt="My project" />            
             <p className="psummary">{summary}</p>
             <p className="ptech">{tech}</p>
-            <a className="plink" href={link}>Link to my GitHub repository</a>                
+            <a className="plink" href={link}>Link to my GitHub repository</a>                            
         </div>
     )
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,60 +8,92 @@ import Popup from "./Popup.jsx"
 
 function Portfolio() {
     const [showProjects, setShowProjects] = useState(false);
-    const [selectedItem, setSelectedItem] = useState(null);
+    const [popup, setPopup] = useState(false);
+    const [popupData, setPopupData] = useState({});
+    const [search, setSearch] = useState("");
+    const projects = [
+        {
+            name: "Form",
+            img: FormSS,
+        },
+        {
+            name: "Grid",
+            img: gridSS,
+        },
+        {
+            name: "Flexbox",
+            img: flexboxSS,
+        }
+    ]
+    
+    const popupDataText = [
+       {
+            name: "Form",
+            img: FormSS,
+            link: "https://github.com/MiniMinaa/",
+            tech: "HTML, CSS",
+            summary: "I built a clean, responsive web form using HTML and CSS. I designed the layout, styling, and user experience, focusing on accessibility and consistency. Throughout the project, I improved form structure, solved alignment issues, and ensured the design works well across all devices."
+       },
+       {
+            name: "Grid",
+            img: gridSS,
+            link: "https://github.com/MiniMinaa/",
+            tech: "HTML, CSS",
+            summary: "I built a layout using CSS Grid. I carefully designed the placement of elements, spacing, and alignment to ensure a clean and consistent visual hierarchy. Throughout the project, I refined the grid structure, solved layout inconsistencies, and optimized it to work seamlessly across different screen sizes and devices. This project helped me deepen my understanding of modern layout techniques and how to create flexible, maintainable designs."
+       },
+       {
+            name: "Flexbox",
+            img: flexboxSS,
+            link: "https://github.com/MiniMinaa/",
+            tech: "HTML, CSS",
+            summary: "I built a website using Flexbox, carefully arranging elements to create a clean and consistent layout. I focused on alignment, spacing, and flexible sizing to ensure the design adapts smoothly to different screen sizes. Throughout the project, I solved layout challenges, refined component positioning, and ensured the interface works well across devices, enhancing both usability and visual consistency."
+        } 
+    ]
 
     return (
         <>
-            {selectedItem && (
-                <Popup 
-                    name={selectedItem.name}
-                    img={selectedItem.img}
-                    link={selectedItem.link}
-                    tech={selectedItem.tech}
-                    summary={selectedItem.summary}
-                ></Popup>
+            {showProjects && (
+                <div className="search-container">
+                    <input className="searchbar"
+                        type="text"
+                        placeholder="Search tag..."
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                </div>
+            )}
+            {showProjects && (
+                <div className="projects">
+                    {projects.filter((project) => (project.name.includes(search)))
+                    
+                    .map((project, index) => (
+                        <ProjectInfo
+                            key={project.name}
+                            name={project.name}
+                            img={project.img}
+                            onClick={() => {
+                                const data = popupDataText.find(p => p.name === project.name);
+                                setPopupData(data);
+                                setPopup(true);
+                            }}
+                        />
+                    ))}
+                </div>
             )}
             {!showProjects && (
                 <div className="button-container">
                     <button className="portfolioButton" onClick={() => setShowProjects(true)}>View my projects!</button> 
                 </div>
             )}
-            {showProjects && (
-                <div className="projects"> 
-                    <ProjectInfo
-                        name="Form"
-                        img={FormSS}
-                        onClick={() => setSelectedItem({
-                            name: "Form",
-                            img: FormSS,
-                            link: "https://github.com/MiniMinaa/Form",
-                            tech: "HTML, CSS",
-                            summary: "I built a clean, responsive web form using HTML and CSS. I designed the layout, styling, and user experience, focusing on accessibility and consistency. Throughout the project, I improved form structure, solved alignment issues, and ensured the design works well across all devices."
-                        })}
-                    />
-                    <ProjectInfo
-                        name="Grid"
-                        img={gridSS}                        
-                        onClick={() => setSelectedItem({
-                            name: "Grid",
-                            img: gridSS,
-                            link: "https://github.com/MiniMinaa/Form",
-                            tech: "HTML, CSS",
-                            summary: "I built a layout using CSS Grid. I carefully designed the placement of elements, spacing, and alignment to ensure a clean and consistent visual hierarchy. Throughout the project, I refined the grid structure, solved layout inconsistencies, and optimized it to work seamlessly across different screen sizes and devices. This project helped me deepen my understanding of modern layout techniques and how to create flexible, maintainable designs."
-                        })}
-                    />
-                    <ProjectInfo
-                        name="Flexbox"
-                        img={flexboxSS}                       
-                        onClick={() => setSelectedItem({
-                            name: "Flexbox",
-                            img: flexboxSS,
-                            link: "https://github.com/MiniMinaa/Form",
-                            tech: "HTML, CSS",
-                            summary: "I built a website using Flexbox, carefully arranging elements to create a clean and consistent layout. I focused on alignment, spacing, and flexible sizing to ensure the design adapts smoothly to different screen sizes. Throughout the project, I solved layout challenges, refined component positioning, and ensured the interface works well across devices, enhancing both usability and visual consistency."
-                        })}
-                    />
-                </div> 
+            {popup && (
+                <Popup
+                    name={popupData.name}
+                    img={popupData.img}
+                    link={popupData.link}
+                    tech={popupData.tech}
+                    summary={popupData.summary}
+                    onClose={() => setPopup(false)}
+                />
             )}
         </>
     )
